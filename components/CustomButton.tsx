b5bcf6a4d2bec5ce204e923d66ec25b394bdfdbc
hasanugr <hasan.ugur4535@gmail.com>
2023-08-13 18:17:26 +0300

    feat: added layout and components as footer,navbar

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -4,12 +4,12 @@ import React from 'react';
 import Image from 'next/image';
 import { CustomButtonProps } from '@/types';
 
-const CustomButton = ({ title, containerStyles, handleClick }: CustomButtonProps) => {
+const CustomButton = ({ title, containerStyles, handleClick, btnType }: CustomButtonProps) => {
   return (
     <button 
       className={`custom-btn ${containerStyles}`}
       disabled={false}
-      type='button'
+      type={btnType || 'button'}
       onClick={handleClick}
     >
       <span className='flex-1'>
