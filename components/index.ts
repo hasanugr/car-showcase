3023c9354d120bfbc7a47b818f7c252cd0c9984b
hasanugr <hasan.ugur4535@gmail.com>
2023-08-13 19:23:44 +0300

    feat: added searchbar component and headlessui

--- components/index.ts
+++ components/index.ts
@@ -2,10 +2,16 @@ import Navbar from './Navbar';
 import Footer from './Footer';
 import Hero from './Hero';
 import CustomButton from './CustomButton';
+import SearchBar from './SearchBar';
+import CustomFilter from './CustomFilter';
+import SearchManufacturer from './SearchManufacturer';
 
 export {
   Navbar,
   Footer,
   Hero,
   CustomButton,
+  SearchBar,
+  CustomFilter,
+  SearchManufacturer as SearchManufacturer,
 };
